00b5b7aae09a51115ab03bfe3fd4ceb6a33151af
phamquan1992 <phamquan2471992@gmail.com>
2023-01-05 11:07:21 +0700

    cm

--- qcglobal.FEW/ClientApp/src/app/components/qlhethong/logsystem/logsystem.component.ts
+++ qcglobal.FEW/ClientApp/src/app/components/qlhethong/logsystem/logsystem.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+export interface item_log {
+  action_name: string;
+  action_time: string;
+  action_dec: string;
+}
 @Component({
   selector: 'app-logsystem',
   templateUrl: './logsystem.component.html',
   styleUrls: ['./logsystem.component.css']
 })
-export class LogsystemComponent {
+export class LogsystemComponent implements OnInit {
+  arr_time: item_log[] = [];
+  ngOnInit(): void {
+    for (let index = 0; index < 10; index++) {
+      let gt_tmp: item_log = {
+        action_name: 'Chức năng ' + index,
+        action_time: '05/01/2023',
+        action_dec: 'Thực hiện chức năng ' + index
+      };
+      this.arr_time.push(gt_tmp);
+    }
+  }
+  xem_them() {
+    for (let index = 0; index < 10; index++) {
+      let gt_tmp: item_log = {
+        action_name: 'Chức năng ' + this.arr_time.length,
+        action_time: '05/01/2023',
+        action_dec: 'Thực hiện chức năng ' + this.arr_time.length
+      };
+      this.arr_time.push(gt_tmp);
+    }
+  }
 
 }
